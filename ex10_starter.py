63e14f141cfdd00d22992f2c8ff3ca9d8caeea95
Stephen Harkins <stephenjharkins@hotmail.co.uk>
2023-06-16 00:12:47 +0100

    update

---
+++ ex10_starter.py
@@ -0,0 +1,30 @@
+import sys, glob, os
+
+# Get the directory name
+if sys.platform == 'win32':
+    hdir = os.environ['HOMEPATH']
+else:
+    hdir = os.environ['HOME']
+
+# Construct a portable wildcard pattern
+pattern = os.path.join(hdir,'*')
+
+# TODO: Use the glob.glob() function to obtain the list of filenames
+
+# TODO: use os.path.getsize to find each file's size
+
+# TODO: Add a test to only display files that are not zero length
+
+# TODO: Remove the leading directory name(s) from each filename before you print it - 
+# using os.path.basename()
+
+print(glob.glob(pattern))
+
+list = glob.glob(pattern)
+
+
+for x in list:
+    if os.path.getsize(x)>0:
+        print(x,os.path.getsize(x))
+
+    print(os.path.getsize(x))
