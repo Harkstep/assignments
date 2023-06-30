0260080c43ded4fff03beafa29e51ba3cca08b38
Stephen Harkins <stephenjharkins@hotmail.co.uk>
2023-06-30 20:32:46 +0100

    cached removal

--- testing page.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-pattern = r'(.*)://(.*)'
-
-text = 'http://example.com'
-matches = re.findall(pattern, text, re.MULTILINE)
-print('Output:')
-print(f'Pattern: {pattern}\nText: {text}\nMatches: {matches}\n')
